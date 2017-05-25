31893e62f1866d602a1ce7d4d083d845a493a265
Jon Seay <git@joncodes.com>
2017-05-24 22:40:07 -0500

    The tiny beginnings of a transactions API.

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const express = require('express')
+
+const app = express()
+app.set('port', (process.env.PORT || 8442))
+
+const transactions = require('./apis/transactions')
+
+app.use('/api/transactions', transactions)
+
+app.listen(app.get('port'), () => {
+    console.log(`wintergreen API now running at: http://localhost:${app.get('port')}/`)
+})
