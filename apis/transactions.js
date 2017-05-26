e59df2c23bc9d91c93a1a4177340811ea48813b3
Jon Seay <git@joncodes.com>
2017-05-26 16:55:12 -0500

    Some hard coded transactions until I get a DB.

--- apis/transactions.js
+++ apis/transactions.js
@@ -4,7 +4,20 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.status(200).send()
+  res.json([
+      {
+        amount: 7.34,
+        date: '2017-05-10',
+        description: '7-11',
+        category: 'Convenience'
+      },
+      {
+        amount: 17.05,
+        date: '2017-05-12',
+        description: 'PDQ',
+        category: 'Gas'
+      }
+  ])
 })
 
 module.exports = router
